13da09af431f780876418a58c686c2500264925b
laurenbottomley <laurenbottomley@icloud.com>
2023-10-15 16:43:37 -0400

    remove unnecessary comments

--- src/scrape.py
+++ src/scrape.py
@@ -18,22 +18,3 @@ data = yf.download(tickers, start='2023-01-01', end='2023-12-31')
 # Save the data to CSV and Excel
 csv_file = data.to_csv('data/output.csv')
 excel_file = data.to_excel('data/output.xlsx')
-
-
-
-# # Calculate daily returns
-# daily_returns = data['Adj Close'].pct_change()
-
-# # Plot the daily returns of a specific company (e.g., Apple)
-# daily_returns['AAPL'].plot()
-# plt.title('Daily Returns of Apple')
-# plt.xlabel('Date')
-# plt.ylabel('Returns')
-# plt.show()
-
-# mean_returns = daily_returns.mean()
-# mean_returns.plot(kind='bar')
-# plt.title('Average Daily Returns for S&P 500 Stocks')
-# plt.xlabel('Stock Ticker')
-# plt.ylabel('Average Daily Return')
-# plt.show()
